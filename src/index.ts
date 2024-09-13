3bdf9af5c3f52860681d6a70152c31589ef9c308
Lara Josue <wilsonlara@esther.ai>
2024-09-13 15:47:46 -0500

    Initialize project and set up Docker
    - Installed necessary dependencies for Express, Fastify, TypeORM, and PostgreSQL.
    - Set up TypeScript configuration (tsconfig.json).
    - Created Dockerfile for the Node.js application.
    - Configured docker-compose to manage both the PostgreSQL database and the Node.js app.

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import morgan from "morgan";
+import cors from "cors";
+import express from "express";
+import * as dotenv from "dotenv";
+import "reflect-metadata";
+
+
+
+// load .env
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Rutas de la API
+app.get('/', (req, res) => {
+  res.send('event management');
+});
+
+app.listen(port, () => {
+  console.log(`Server on port:  ${port}`);
+});
